1bda6a5594d5776ee4bc74ce3ceac11fa86b9d15
Angelo Rossi <angelo.rossi@blackboard.com>
2016-07-17 23:10:56 +1000

    build failed on heroku

--- acacia_main/settings.py
+++ acacia_main/settings.py
@@ -162,6 +162,7 @@ CACHES = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+DISABLE_COLLECTSTATIC=1
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
